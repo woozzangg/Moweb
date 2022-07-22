675ca47e5e490149cd96dd686b42a77c51505bae
정태윤 <tanficial9574@gmail.com>
2022-07-22 16:41:35 +0900

    Init : 백엔드 프로젝트 생성

---
+++ moweb_back/src/main/java/com/a507/moweb/MowebBackApplication.java
@@ -0,0 +1,13 @@
+package com.a507.moweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowebBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MowebBackApplication.class, args);
+    }
+
+}
