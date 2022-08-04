dd9c0be9b5159ce2907cc02d35b3dbb2e6e4f307
김우창 <dannba1@naver.com>
2022-08-04 11:39:22 +0900

    Update: vuetify install

---
+++ moweb_front/src/views/ResultView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="result1">
+    <body>
+      <h1>결과 화면</h1>
+    </body>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ResultView",
+};
+</script>
+
+<style>
+.mint {
+  background-color: #12d3a9;
+}
+.choco {
+  background-color: #563d34;
+}
+.background {
+  height: 500vh;
+  overflow: hidden;
+  margin: 0;
+  background-image: url("@/assets/mainpage.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+</style>
---
+++ moweb_front/src/views/WaitingView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="waiting1">
+    <body>
+      <h1>촬영 대기</h1>
+    </body>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WaitingView",
+};
+</script>
+
+<style>
+.mint {
+  background-color: #12d3a9;
+}
+.choco {
+  background-color: #563d34;
+}
+.background {
+  height: 500vh;
+  overflow: hidden;
+  margin: 0;
+  background-image: url("@/assets/mainpage.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+</style>
