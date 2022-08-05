83de804cbc808ce30094f2298426612ab9bf024a
김우창 <dannba1@naver.com>
2022-08-05 11:02:53 +0900

    Update: 브랜치 이동 위해 현재 작업 옮김

--- moweb_front/src/views/WaitingView.vue
+++ moweb_front/src/views/WaitingView.vue
@@ -1,9 +1,26 @@
 <template>
-  <div class="waiting1">
-    <body>
-      <h1>촬영 대기</h1>
-    </body>
-  </div>
+  <v-container class="grey lighten-5">
+    <v-row rows="12">
+      <!-- no-gutters -->
+      <v-col cols="9">
+        <v-card class="pa-2" outlined tile height="60vh"> 컴포 </v-card>
+      </v-col>
+      <v-col cols="3">
+        <v-row class="pa-2" rows="6" cols="3">
+          <v-card class="ma-2" outlined tile height="25vh" weight="10vh">
+            참여자
+          </v-card>
+        </v-row>
+        <v-row rows="7">
+          <v-card class="pa-0" outlined tile height="35vh"> 채팅창</v-card>
+        </v-row>
+        <!-- <v-card class="pa-2" outlined tile height="70vh">
+          <v-row rows="9">123</v-row>
+          <v-row rows="3">11</v-row>
+        </v-card> -->
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
