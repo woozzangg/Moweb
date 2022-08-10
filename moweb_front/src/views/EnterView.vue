5a1c4b683589a72a952e162741dc5c3afb119f1a
김승준 <joon8445@naver.com>
2022-08-10 16:31:29 +0900

    Update : 방. 닉네임 유효성 검사, enterView에 맞춰서 배치

--- moweb_front/src/views/EnterView.vue
+++ moweb_front/src/views/EnterView.vue
@@ -1,50 +1,179 @@
 <template>
   <v-container class="border-style1" d-flex justify-space-around>
+    <video v-show="false" ref="input_video"></video>
     <v-col cols="10">
-      <div style="margin: 50px 0px 0px 0px">
-        <div class="centercss">
-          <canvas
-            class="border-style1"
-            width="640"
-            height="480"
-            style="background-color: #ff0000; margin: 20px 0px 0px 50px"
-          ></canvas>
-        </div>
-        <div class="centercss">
-          <v-input>닉네임 입력창</v-input>
-        </div>
-        <div>
-          <v-row
-            fluid
-            no-gutters
-            rows="2"
-            class="border-style1"
-            style="margin: 4px"
-          >
-            버튼 모음집
-            <v-btn elevation="10" outlined tile rounded>
-              <router-link to="/shot" style="margin: 10px"
-                >shot으로</router-link
-              >
-
-              <router-view />
-            </v-btn>
-            <v-btn class="pink white--text">
-              <router-link to="/waiting" style="margin: 10px"
-                >waiting으로</router-link
-              >
-              |
-            </v-btn>
-          </v-row>
-        </div>
-      </div>
+      <v-row>
+        <canvas
+          class="output_canvas"
+          id="output_canvas"
+          :width="width"
+          :height="height"
+          style="transform: rotateY(180deg); margin: auto"
+        ></canvas>
+      </v-row>
+      <v-row class="centercss">
+        <input
+          placeholder="닉네임 입력"
+          v-model="user_name"
+          style="border-style: solid"
+        />
+        <input
+          placeholder="url, join테스트 추후 삭제예정"
+          v-model="url"
+          style="border-style: solid"
+        />
+      </v-row>
+      <v-row
+        fluid
+        no-gutters
+        rows="2"
+        class="border-style1"
+        style="margin: 4px"
+      >
+        버튼 모음집
+        <v-btn @click="createRoom">방만들기</v-btn>
+        <v-btn @click="joinRoom">방입장하기</v-btn>
+      </v-row>
     </v-col>
   </v-container>
 </template>
 
 <script>
+import { Camera } from "@mediapipe/camera_utils";
+import { SelfieSegmentation } from "@mediapipe/selfie_segmentation";
+import axios from "axios";
+
+const API_URL = "https://i7a507.p.ssafy.io/moweb-api";
+
 export default {
   name: "EnterView",
+  data() {
+    return {
+      width: 720,
+      height: 540,
+      user_name: "",
+      url: "",
+    };
+  },
+  computed: {
+    inputVideo() {
+      return this.$refs.input_video;
+    },
+  },
+  mounted() {
+    this.removeBG();
+    this.url = this.$route.params.url;
+  },
+  methods: {
+    createRoom() {
+      axios
+        .post(
+          API_URL + "/room/create",
+          JSON.stringify({
+            user_name: this.user_name,
+          })
+        )
+        .then(({ data }) => {
+          if (data.room_no > 0) {
+            this.$router.push({
+              name: "webrtc",
+              params: {
+                user_name: this.user_name,
+                room_no: data.room_no,
+                url: data.url,
+              },
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    joinRoom() {
+      axios
+        .post(
+          API_URL + "/room/join",
+          JSON.stringify({
+            user_name: this.user_name,
+            url: this.url,
+          })
+        )
+        .then(({ data }) => {
+          if (data.room_no == -2) {
+            alert("유효하지 않은 방입니다.");
+            this.$router.replace({ name: "main" });
+          } else if (data.room_no == -1) {
+            alert("방 인원이 가득찼습니다.");
+          } else if (data.room_no == 0) {
+            alert("이름이 중복되었습니다.");
+          }
+          if (data.room_no > 0) {
+            this.camera.stop();
+            this.$router.push({
+              name: "webrtc",
+              params: {
+                user_name: this.user_name,
+                room_no: data.room_no,
+                url: data.url,
+              },
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    async removeBG() {
+      this.ctx = document.getElementById("output_canvas").getContext("2d");
+      const selfieSegmentation = new SelfieSegmentation({
+        locateFile: (file) => {
+          return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
+        },
+      });
+      selfieSegmentation.setOptions({
+        modelSelection: 1,
+      });
+      selfieSegmentation.onResults(this.onResults);
+
+      this.camera = new Camera(this.inputVideo, {
+        onFrame: async () => {
+          await selfieSegmentation.send({ image: this.inputVideo });
+        },
+        width: 720,
+        height: 540,
+      });
+      this.camera.start();
+
+      return 1;
+    },
+
+    // 배경제거 옵션
+    async onResults(results) {
+      this.width = results.image.width;
+      this.height = results.image.height;
+      this.ctx.save();
+      this.ctx.clearRect(0, 0, results.image.width, results.image.height);
+      this.ctx.drawImage(
+        results.segmentationMask,
+        0,
+        0,
+        results.image.width,
+        results.image.height
+      );
+
+      // Only overwrite missing pixels.
+      this.ctx.globalCompositeOperation = "source-in";
+      await this.ctx.drawImage(
+        results.image,
+        0,
+        0,
+        results.image.width,
+        results.image.height
+      );
+
+      this.ctx.restore();
+    },
+  },
 };
 </script>
 
