a3418bd3af15ed7085967df1203f0056e497e0a1
Kim Sohyun <janeqcitizen@naver.com>
2022-08-12 18:05:11 +0900

    Add: 메인 페이지 완성

--- moweb_front/src/App.vue
+++ moweb_front/src/App.vue
@@ -11,23 +11,9 @@
     <router-view />
   </div>
 </template>
+<script></script>
 
 <style>
-/* #app { */
-/* font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50; */
-/* height: 100vh;
-  overflow: hidden;
-  margin: 0; */
-/* background-image: url("./assets/background.jpg"); */
-/* background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center; */
-/* } */
-
 nav {
   padding: 30px;
 }
@@ -39,5 +25,52 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+.v-application {
+  font-family: "SCDream", "NanumGgeu", sans-serif !important;
+}
+@font-face {
+  font-family: "SCDream";
+  src: url(@/assets/fonts/SCDream1.otf);
+  font-weight: 1;
+}
+@font-face {
+  font-family: "SCDream";
+  src: url(@/assets/fonts/SCDream2.otf);
+  font-weight: 2;
+}
+@font-face {
+  font-family: "SCDream";
+  src: url(@/assets/fonts/SCDream3.otf);
+  font-weight: 3;
+}
+@font-face {
+  font-family: "SCDream";
+  src: url(@/assets/fonts/SCDream4.otf);
+  font-weight: 4;
+}
+@font-face {
+  font-family: "SCDream";
+  src: url(@/assets/fonts/SCDream5.otf);
+  font-weight: 5;
+}
+@font-face {
+  font-family: "SCDream";
+  src: url(@/assets/fonts/SCDream6.otf);
+  font-weight: 6;
+}
+@font-face {
+  font-family: "SCDream";
+  src: url(@/assets/fonts/SCDream7.otf);
+  font-weight: 8;
+}
+@font-face {
+  font-family: "SCDream";
+  src: url(@/assets/fonts/SCDream9.otf);
+  font-weight: 9;
+}
+@font-face {
+  font-family: "NanumGgeu";
+  src: url(@/assets/fonts/NanumGgeuTeuMeoRiCe.ttf);
 }
 </style>
