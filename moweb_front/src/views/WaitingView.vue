55df058c16b26db28c1a36a22eef5c886fbf7d53
김승준 <joon8445@naver.com>
2022-08-16 15:53:30 +0900

    Fix : shot 여러번 버그.

--- moweb_front/src/views/WaitingView.vue
+++ moweb_front/src/views/WaitingView.vue
@@ -253,8 +253,10 @@
                   :dialogProp="shotDialog"
                   :count="count"
                   :isAdmin="is_admin"
+                  :shotBtnActive="shotBtnActive"
                   @sendShotCountdown="sendShotCountdown"
                   @sendDialogChange="sendDialogChange"
+                  @shotBtndisActive="shotBtnActive = false"
                 >
                   <layered-video
                     width="960"
@@ -520,9 +522,7 @@ export default {
         case 7:
           this.shot_cnt = content.shot_cnt;
           await this.takepic();
-          // if (this.shot_cnt === 4) {
-          //   this.page2Result();
-          // }
+          this.shotBtnActive = true;
           break;
         // 방장이 나감
         case 8:
@@ -1059,8 +1059,6 @@ export default {
     async takepic() {
       await this.pictureBackground(); // 각자의 사진으로 할때
       let canvas = this.$refs["personal_canvas"]; // 각자의 사진으로 할때
-      // if (!this.is_admin) return; // 공유화면에서 사진찍을 때
-      // const canvas = this.$refs.layeredVideo.$refs.layeredOutputCanvas;
       const imgBase64 = canvas.toDataURL("image/png");
       const decodImg = atob(imgBase64.split(",")[1]);
 
